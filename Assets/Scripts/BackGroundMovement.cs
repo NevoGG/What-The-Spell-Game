4db4ea8756bbe1b4b682f23fd3fa10057d04dfd6
fedebie <fedebie@gmail.com>
2022-12-24 16:19:57 +0200

    fix

--- Assets/Scripts/BackGroundMovement.cs
+++ Assets/Scripts/BackGroundMovement.cs
@@ -10,10 +10,7 @@ public class BackGroundMovment : MonoBehaviour
 
     private bool acend = true;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
