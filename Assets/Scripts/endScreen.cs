7b3a12eb286bce6e1831333aa7910659bd849892
roee <roee.stoffman@mail.huji.ac.il>
2023-01-04 13:17:19 +0200

    Fixed timer bug and add buttons for play again and main menu

--- Assets/Scripts/endScreen.cs
+++ Assets/Scripts/endScreen.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class endScreen : MonoBehaviour
 {
     
     [SerializeField] GameObject mainCam;
     [SerializeField] private List<Vector3> positions;
+    [SerializeField] private GameObject buttonMainMenu;
+    [SerializeField] private GameObject buttonPlayAgain;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCam.transform.position = new Vector3(transform.position.x, transform.position.y,
             mainCam.transform.position.z);
+        buttonMainMenu.SetActive(true);
+        buttonPlayAgain.SetActive(true);
     }
 
     public void EndGame(List<Player> rankList)
@@ -37,6 +42,16 @@ public class endScreen : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("OpeningScene");
+    }
+    
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
     
     
