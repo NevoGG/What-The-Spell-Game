9421d7ea7e996e70ae8ee7b37fe1d930f4a67868
fedebie <fedebie@gmail.com>
2023-01-06 14:07:36 +0200

    DONOT UPDATE THIS COMMIT made a boobo and worked in main instad of fede, am pushingg it only to move it to fede and the to roll it back

---
+++ Assets/Scripts/UiControler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiControler : MonoBehaviour
+{
+    [SerializeField] private GameObject player1Img;
+    [SerializeField] private GameObject player2Img;
+    [SerializeField] private GameObject player3Img;
+    [SerializeField] private GameObject player4Img;
+    [SerializeField] private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
