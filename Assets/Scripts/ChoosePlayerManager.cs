bed45d033012fd1b8407dd754356317c257c7c5e
NevoGG <97809610+NevoGG@users.noreply.github.com>
2023-01-17 22:32:50 +0200

    All is bunny

--- Assets/Scripts/ChoosePlayerManager.cs
+++ Assets/Scripts/ChoosePlayerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -9,7 +10,30 @@ public class ChoosePlayerManager : MonoBehaviour
 
     public static List<bool> playersArr = new List<bool>{false, false, false, false};
     public static int numberOfPlayers = 0;
-
+    private PlayerInput1 controls1;
+    protected InputAction move1;
+    protected InputAction jump1;
+    protected InputAction crouch1;
+    protected InputAction dash1;
+    
+    private PlayerInput2 controls2;
+    protected InputAction move2;
+    protected InputAction jump2;
+    protected InputAction crouch2;
+    protected InputAction dash2;
+    
+    private PlayerInput3 controls3;
+    protected InputAction move3;
+    protected InputAction jump3;
+    protected InputAction crouch3;
+    protected InputAction dash3;
+    
+    private PlayerInput4 controls4;
+    protected InputAction move4;
+    protected InputAction jump4;
+    protected InputAction crouch4;
+    protected InputAction dash4;
+    
     [SerializeField] private Image redKeysRenderer;
     [SerializeField] private Image blueKeysRenderer;
     [SerializeField] private Image greenKeysRenderer;
@@ -24,14 +48,100 @@ public class ChoosePlayerManager : MonoBehaviour
     [SerializeField] private Sprite greenKeys;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        controls1 = new PlayerInput1();
+        controls2 = new PlayerInput2();
+        controls3 = new PlayerInput3();
+        controls4 = new PlayerInput4();
     }
-
+    
+    private void OnEnable()
+    {
+        move1 = controls1.Player.Move;
+        move1.Enable();
+        jump1 = controls1.Player.Jump;
+        jump1.Enable();
+        crouch1 = controls1.Player.Crouch;
+        crouch1.Enable();
+        dash1 =controls1.Player.Dash;
+        dash1.Enable();
+        move1.performed += ActivatePlayer1;
+        dash1.performed += ActivatePlayer1;
+        jump1.performed += ActivatePlayer1;
+        crouch1.performed += ActivatePlayer1;
+        
+        move2 = controls2.Player.Move;
+        move2.Enable();
+        jump2 = controls2.Player.Jump;
+        jump2.Enable();
+        crouch2 = controls2.Player.Crouch;
+        crouch2.Enable();
+        dash2 =controls2.Player.Dash;
+        dash2.Enable();
+        move2.performed += ActivatePlayer2;
+        dash2.performed += ActivatePlayer2;
+        jump2.performed += ActivatePlayer2;
+        crouch2.performed += ActivatePlayer2;
+        
+        move3 = controls3.Player.Move;
+        move3.Enable();
+        jump3 = controls3.Player.Jump;
+        jump3.Enable();
+        crouch3 = controls3.Player.Crouch;
+        crouch3.Enable();
+        dash3 =controls3.Player.Dash;
+        dash3.Enable();
+        move3.performed += ActivatePlayer3;
+        dash3.performed += ActivatePlayer3;
+        jump3.performed += ActivatePlayer3;
+        crouch3.performed += ActivatePlayer3;
+        
+        move4 = controls4.Player.Move;
+        move4.Enable();
+        jump4 = controls4.Player.Jump;
+        jump4.Enable();
+        crouch4 = controls4.Player.Crouch;
+        crouch4.Enable();
+        dash4 =controls4.Player.Dash;
+        dash4.Enable();
+        move4.performed += ActivatePlayer4;
+        dash4.performed += ActivatePlayer4;
+        jump4.performed += ActivatePlayer4;
+        crouch4.performed += ActivatePlayer4;
+    }
+    
+    private void OnDisable()
+    {
+        move1.Disable();
+        jump1.Disable();
+        dash1.Disable();
+        crouch1.Disable();
+        
+        move2.Disable();
+        jump2.Disable();
+        dash2.Disable();
+        crouch2.Disable();
+        
+        move3.Disable();
+        jump3.Disable();
+        dash3.Disable();
+        crouch3.Disable();
+        
+        move4.Disable();
+        jump4.Disable();
+        dash4.Disable();
+        crouch4.Disable();
+    }
+    
     // Update is called once per frame
     void Update()
     {
+        // if (move1.ReadValue<float>() != 0) ActivatePlayer1();
+        // if(move1.ReadValue<float>() != 0)
+        // if(move1.ReadValue<float>() != 0) 
+        //     if(move1.ReadValue<float>() != 0)
+        
         if (Input.GetKey(KeyCode.Escape))
         {
             ResetPlayers();
@@ -41,7 +151,7 @@ public class ChoosePlayerManager : MonoBehaviour
             SceneManager.LoadScene("SampleScene");
         }
 
-        check();
+        // check();
         countPlayers();
     }
 
@@ -57,25 +167,25 @@ public class ChoosePlayerManager : MonoBehaviour
         }
     }
 
-    private void check()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            ActivatePlayer1();
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            ActivatePlayer2();
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            ActivatePlayer3();
-        }
-        if (Input.GetKey(KeyCode.F))
-        {
-            ActivatePlayer4();
-        }
-    }
+    // private void check()
+    // {
+    //     if (Input.GetKey(KeyCode.A))
+    //     {
+    //         ActivatePlayer1();
+    //     }
+    //     if (Input.GetKey(KeyCode.S))
+    //     {
+    //         ActivatePlayer2();
+    //     }
+    //     if (Input.GetKey(KeyCode.D))
+    //     {
+    //         ActivatePlayer3();
+    //     }
+    //     if (Input.GetKey(KeyCode.F))
+    //     {
+    //         ActivatePlayer4();
+    //     }
+    // }
 
     private void ResetPlayers()
     {
@@ -89,25 +199,25 @@ public class ChoosePlayerManager : MonoBehaviour
         yellowKeysRenderer.sprite = greyYellowKeys;
     }
 
-    public void ActivatePlayer1()
+    public void ActivatePlayer1(InputAction.CallbackContext context)
     {
         redKeysRenderer.sprite = redKeys;
         playersArr[0] = true;
     }
     
-    public void ActivatePlayer2()
+    public void ActivatePlayer2(InputAction.CallbackContext context)
     {
         greenKeysRenderer.sprite = greenKeys;
         playersArr[1] = true;
     }
     
-    public void ActivatePlayer3()
+    public void ActivatePlayer3(InputAction.CallbackContext context)
     {
         blueKeysRenderer.sprite = blueKeys;
         playersArr[2] = true;
     }
     
-    public void ActivatePlayer4()
+    public void ActivatePlayer4(InputAction.CallbackContext context)
     {
         yellowKeysRenderer.sprite = yellowKeys;
         playersArr[3] = true;
