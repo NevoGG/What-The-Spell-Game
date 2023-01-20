12bc2fef971ac0a6506f7fda005d03644bdbcd21
NevoGG <97809610+NevoGG@users.noreply.github.com>
2023-01-20 13:46:29 +0200

    Arrow files

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private GameObject toFollow;
+    private Camera _camera;
+    private Animator _animator;
+    private float upY;
+    private float downY;
+    private bool isFlipped = false;
+    private float fallThreshold = 0.2f;
+    
+
+    private float _distFromEdge = (CameraMovment.cameraWidth / 2) * (3f/4f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = Camera.main;
+        _animator = GetComponent<Animator>();
+        upY =  12f;
+        downY = CameraMovment.cameraWidth - _distFromEdge;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = _camera.WorldToViewportPoint(toFollow.transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+        if (!onScreen)
+        {
+            _animator.SetBool("IsOutOfScreen", true);
+             transform.localPosition = new Vector3(toFollow.transform.position.x, upY, screenPoint.z);
+             if (screenPoint.y > 1 + fallThreshold)
+             {
+                 _animator.SetTrigger("DeadUp");
+             }
+             if(screenPoint.y < 0 - fallThreshold){}
+        }
+        else
+        {
+            _animator.SetBool("IsOutOfScreen", false);
+        }
+    }
+}
